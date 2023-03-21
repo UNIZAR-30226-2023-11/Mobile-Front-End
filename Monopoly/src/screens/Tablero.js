de7b006f069aefdc9e32eb771a88d304f043ba4c
Luna Álvarez <815976@unizar.es>
2023-03-21 08:54:15 +0100

    Estructura del tablero completada, falta rellenar las casillas con los datos

--- Monopoly/src/screens/Tablero.js
+++ Monopoly/src/screens/Tablero.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, StyleSheet } from'react-native';
 import StyledText from '../components/StyledText';
 
+const ancho = 41.2;
+
 const styles = StyleSheet.create({
     header:{
         flex: 1,
@@ -10,39 +12,48 @@ const styles = StyleSheet.create({
     },
     tablero:{
         backgroundColor: '#FFFFFF',
-        flex:50,
+        flex:4,
         flexDirection: 'column'
     },
+    cursos3_1:{
+        flex: 5,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
     curso1:{
+        position: 'absolute',
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'flex-end',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginLeft: ancho,
+        marginTop: ancho*9
     },
     curso2: {
-        alignItems: 'flex-start',
+        position: 'absolute',
+        justifyContent: 'center',
         flex: 1,
         flexDirection:'column',
-        marginTop:'33.5%'
     },
     curso3:{
+        position: 'relative',
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'flex-start',
         flexDirection: 'row',
-        marginBottom:'100%'
+        marginLeft: ancho,
     },
     curso4:{
+        position: 'relative',
         alignItems: 'flex-end',
         flex: 1,
         flexDirection:'column'
     },
     space:{
-        width:'90%'
+        alignSelf: 'stretch',
+        backgroundColor: '#EAEAEA'
     },
     casilla:{
-        width: 36,
-        height: 36,
+        width: ancho,
+        height: ancho,
         borderColor: '#000000',
         borderWidth: 1
     }
@@ -53,7 +64,7 @@ export default function TableroScreen() {
     return (
         <View style={{flex:1,flexDirection:'column'}}>
         <View style={styles.header}>
-            <StyledText blue bold> MONOPOLY </StyledText>
+            <StyledText blue bold big> MONOPOLY </StyledText>
         </View>
         <View style={styles.tablero}>
             <View style={styles.curso2}>
@@ -88,67 +99,69 @@ export default function TableroScreen() {
             
                 </View>
             </View>
-            <View style={styles.curso3}>
-                <View style={styles.casilla}>
-                    
+            <View style={styles.cursos1_3}>
+                <View style={styles.curso3}>
+                    <View style={styles.casilla}>
+                        
+                    </View>
+                    <View style={styles.casilla}>
+                        
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
                 </View>
-                <View style={styles.casilla}>
-                    
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
+                <View style={styles.curso1}>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
+                    <View style={styles.casilla}>
+                
+                    </View>
                 </View>
             </View>
             <View style={styles.curso4}>
                 <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-                <View style={styles.casilla}>
-            
-                </View>
-            </View>
-            <View style={styles.curso1}>
-                <View style={styles.casilla}>
                 
+                </View>
+                <View style={styles.casilla}>
+            
+                </View>
+                <View style={styles.casilla}>
+            
                 </View>
                 <View style={styles.casilla}>
             
