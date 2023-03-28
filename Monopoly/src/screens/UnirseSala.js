9b8043d2281f5056ea841b3d1b6d249a972af546
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2023-03-28 20:30:19 +0200

    Implementado todo sin BD
    Falta mostrar el boton del modal que lleve a CrearSala. El codigo está presente pero el botón no se muestra.
    Finalmente habría que conectar a la BD para buscar el id de la partida

--- Monopoly/src/screens/UnirseSala.js
+++ Monopoly/src/screens/UnirseSala.js
@@ -1,21 +1,63 @@
 import React from 'react';
-import { View, StyleSheet } from'react-native';
+import { View, StyleSheet , Text , Button, Pressable} from'react-native';
 import {Searchbar} from 'react-native-paper';
+import StyledModal from "../components/StyledModal";
+import StyledButton from "../components/StyledButton";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const styles = StyleSheet.create({
     barra: { 
         padding: 15,
+    },
+    titulo: {
+        fontSize: 25,
+        marginBottom: '5%',
+        marginTop: '5%',
+        alignContent: 'center',
+        alignSelf: 'center',
+    },
+    unirme: {
+
+    },
+    modal: {
+        height: '30%',
+        width: '84%',
     }
 });
 
 export default function UnirseSalaScreen({ navigation }) {
 
+    const [modalPartidaVisible, setModalPartidaVisible] = React.useState(false);
+
     return (
         <View style={styles.barra}>
+            <Text style={styles.titulo}> Introduce el id de la partida</Text>
             <Searchbar
-                placeholder="Buscar partida (#123456)"
+                placeholder="123456"
+                placeholderTextColor="grey"
+                onSubmitEditing={() => setModalPartidaVisible(true)}
             />
+
+            <StyledModal
+                title="Únete a la partida"
+                text="Partida #123456"   
+                style={styles.modal}
+                onClose = { () => {setModalPartidaVisible({setModalPartidaVisible: !modalPartidaVisible})}}
+                visible={modalPartidaVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalReglasVisible({modalPartidaVisible: !modalPartidaVisible});
+                }} 
+            >
+                <View>
+                    <Pressable
+                    title="Unirme"
+                    onPress={() => navigation.navigate('CrearSala')}
+                    />
+                </View>
+            </StyledModal> 
+
         </View>
     );
-}
+};
