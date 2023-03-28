d722c3591292be1546347a64cf5aae66f00c829f
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2023-03-28 13:11:03 +0200

    Barra de busqueda de UnirseSala
    Faltaría conectar con la base de datos y realizar otra pantalla/alert para que dada la partida, muestre un botoón para unirse a la sala.
    
    Además, queda estilar colores

--- Monopoly/src/screens/UnirseSala.js
+++ Monopoly/src/screens/UnirseSala.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { View, Text } from'react-native';
+import {Searchbar} from 'react-native-paper';
+
+
+const styles = StyleSheet.create({
+    barra: { 
+        padding: 15,
+    }
+});
 
 export default function UnirseSalaScreen({ navigation }) {
 
     return (
-        <View>
-            <Text>Unirse Sala Screen</Text>
+        <View style={StyleSheet.barra}>
+            <Searchbar
+            styled
+                placeholder="Buscar partida (#123456)"
+            />
         </View>
     );
 }
