634e458fb4b6853323f3a352e6bff19e4ab09750
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2023-04-03 19:44:21 +0200

    Update users.js

--- Monopoly/src/url/users.js
+++ Monopoly/src/url/users.js
@@ -1,2 +1,8 @@
 export const registro = 'https://monopoly-inoformatico.azurewebsites.net/users/register'
 export const login = 'https://monopoly-inoformatico.azurewebsites.net/users/login'
+
+export const updateUsuario = 'https://monopoly-inoformatico.azurewebsites.net/users/updateUsername'
+export const updateCorreoUsuario = 'https://monopoly-inoformatico.azurewebsites.net/users/updateCorreo'
+
+export const updatePassword = 'https://monopoly-inoformatico.azurewebsites.net/users/updatePassword'
+export const deleteUsuario = 'https://monopoly-inoformatico.azurewebsites.net/users/delete'
