1bf4f00cd2ba6a1192d91eaeae95c9e7e42d6b25
lunaalvareez <815976@unizar.es>
2023-05-05 15:57:21 +0200

    conectado con el server

--- Monopoly/src/url/partida.js
+++ Monopoly/src/url/partida.js
@@ -1,41 +1,41 @@
-// export const crearPartida = 'http://nerks.net:7003/partida/crear'
-export const crearPartida = 'http://10.0.2.2:3000/partida/crear'
+export const crearPartida = 'http://nerks.net:7003/partida/crear'
+//export const crearPartida = 'http://10.0.2.2:3000/partida/crear'
 
-// export const actualizarPartida = 'http://nerks.net:7003/partida/actualizar'
-export const actualizarPartida = 'http://10.0.2.2:3000/partida/actualizar'
+export const actualizarPartida = 'http://nerks.net:7003/partida/actualizar'
+//export const actualizarPartida = 'http://10.0.2.2:3000/partida/actualizar'
 
-// export const unirPartida = 'http://nerks.net:7003/partida/unirJugador'
-export const unirPartida = 'http://10.0.2.2:3000/partida/unirJugador'
+export const unirPartida = 'http://nerks.net:7003/partida/unirJugador'
+//export const unirPartida = 'http://10.0.2.2:3000/partida/unirJugador'
 
-// export const lanzarDados = 'http://nerks.net:7003/partida/lanzarDados'
-export const lanzarDados = 'http://10.0.2.2:3000/partida/lanzarDados'
+export const lanzarDados = 'http://nerks.net:7003/partida/lanzarDados'
+//export const lanzarDados = 'http://10.0.2.2:3000/partida/lanzarDados'
 
-// export const listaJugadores = 'http://nerks.net:7003/partida/listaJugadores'
-export const listaJugadores = 'http://10.0.2.2:3000/partida/listaJugadores'
+export const listaJugadores = 'http://nerks.net:7003/partida/listaJugadores'
+//export const listaJugadores = 'http://10.0.2.2:3000/partida/listaJugadores'
 
-// export const comprarAsignatura = 'http://nerks.net:7003/partida/comprar' //put
-export const comprarAsignatura = 'http://10.0.2.2:3000/partida/comprar' //put
+export const comprarAsignatura = 'http://nerks.net:7003/partida/comprar' //put
+//export const comprarAsignatura = 'http://10.0.2.2:3000/partida/comprar' //put
 
-// export const infoAsignatura = 'http://nerks.net:7003/partida/infoAsignatura' //put
-export const infoAsignatura = 'http://10.0.2.2:3000/partida/infoAsignatura' //put
+export const infoAsignatura = 'http://nerks.net:7003/partida/infoAsignatura' //put
+//export const infoAsignatura = 'http://10.0.2.2:3000/partida/infoAsignatura' //put
 
-// export const casillaComprada = 'http://nerks.net:7003/partida/casilla' //put
-export const casillaComprada = 'http://10.0.2.2:3000/partida/casilla' //put
+export const casillaComprada = 'http://nerks.net:7003/partida/casilla' //put
+//export const casillaComprada = 'http://10.0.2.2:3000/partida/casilla' //put
 
-// export const tarjeta = 'http://nerks.net:7003/partida/tarjeta'
-export const tarjetaAleatoria = 'http://10.0.2.2:3000/partida/tarjeta'
+export const tarjeta = 'http://nerks.net:7003/partida/tarjeta'
+//export const tarjetaAleatoria = 'http://10.0.2.2:3000/partida/tarjeta'
 
-//export const listarAsignaturas = 'http://nerks.net:7003/partida/listaAsignaturas'
-export const listarAsignaturas = 'http://10.0.2.2:3000/partida/listaAsignaturasC'
+export const listarAsignaturas = 'http://nerks.net:7003/partida/listaAsignaturas'
+//export const listarAsignaturas = 'http://10.0.2.2:3000/partida/listaAsignaturasC'
 
-//export const siguienteTurno = 'http://nerks.net:7003/partida/siguienteTurno'
-export const siguienteTurno = 'http://10.0.2.2:3000/partida/siguienteTurno'
+export const siguienteTurno = 'http://nerks.net:7003/partida/siguienteTurno'
+//export const siguienteTurno = 'http://10.0.2.2:3000/partida/siguienteTurno'
 
-// export const obtenerTurnoActual = 'http://nerks.net:7003/partida/turnoActual'
-export const obtenerTurnoActual = 'http://10.0.2.2:3000/partida/turnoActual'
+export const obtenerTurnoActual = 'http://nerks.net:7003/partida/turnoActual'
+//export const obtenerTurnoActual = 'http://10.0.2.2:3000/partida/turnoActual'
 
-// export const aumentarCreditos = 'http://nerks.net:7003/partida/aumentar'
-export const aumentarCreditosAsignatura = 'http://10.0.2.2:3000/partida/aumentar'
+export const aumentarCreditos = 'http://nerks.net:7003/partida/aumentar'
+//export const aumentarCreditosAsignatura = 'http://10.0.2.2:3000/partida/aumentar'
 
-// export const venderAsignatura = 'http://nerks.net:7003/partida/aumentar'
-export const venderAsignatura = 'http://10.0.2.2:3000/partida/vender'
+export const venderAsignatura = 'http://nerks.net:7003/partida/aumentar'
+//export const venderAsignatura = 'http://10.0.2.2:3000/partida/vender'
--- Monopoly/src/url/users.js
+++ Monopoly/src/url/users.js
@@ -1,7 +1,7 @@
-// export const registro = 'http://nerks.net:7003/users/register'
-export const registro = 'http://10.0.2.2:3000/users/register'
-// export const login = 'http://nerks.net:7003/users/login'
-export const login = 'http://10.0.2.2:3000/users/login'
+export const registro = 'http://nerks.net:7003/users/register'
+// export const registro = 'http://10.0.2.2:3000/users/register'
+export const login = 'http://nerks.net:7003/users/login'
+// export const login = 'http://10.0.2.2:3000/users/login'
 
 export const updateUsuario = 'http://nerks.net:7003/users/updateUsername'
 export const updateCorreoUsuario = 'http://nerks.net:7003/users/updateCorreo'
