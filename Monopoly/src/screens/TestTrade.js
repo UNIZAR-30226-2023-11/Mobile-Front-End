4ba1ddcec7a921b101f06552de243bbff2dd2b00
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2023-05-07 12:31:47 +0200

    Muestra modal trades

--- Monopoly/src/screens/TestTrade.js
+++ Monopoly/src/screens/TestTrade.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { StyleSheet, Button, View, Modal, ScrollView, Text, Select, Pressable } from 'react-native';
+import { StyleSheet, Button, View, Modal, ScrollView, Text, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Select, NativeBaseProvider } from "native-base";
 import StyledButton from "../components/StyledButton";
 import { Entypo } from '@expo/vector-icons';
 
@@ -9,6 +10,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "white",
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        marginTop: 40,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width:'45%',
+        height: '40%',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+        width: 0,
+        height: 2,
+        }
     },
 });
 
@@ -26,29 +46,37 @@ export default function TestTrade(){
     const handleJugadorElegido = (jugador) => {
         setJugadorElegido(jugador);
     };
-    
-    const options = jugadores.map((jugador) => 
-    <option key={jugador} value={jugador}><Text>{jugador}</Text></option>);
 
+    const jugadoresItems = jugadores.map((jugador, i) => (
+        <Select.Item key={i} label={jugador} value={jugador} />
+      ));
 
     return (
         <View>
             <StyledButton title="TRADE" onPress={() => setModalVisible(true)} />
-
+        <NativeBaseProvider>
             <Modal style={styles.modalView} visible={modalVisible}>
                 <View style={styles.centeredView}>
                     <Pressable
-                    onPress={() => setModalVisible(false)}>
+                        onPress={() => setModalVisible(false)}>
                         <Entypo name="circle-with-cross" size={35} color="red" style={styles.button}/>
                     </Pressable>
 
                     <Text>Crear intercambio con:</Text>
-                    <select value={jugadorElegido} onChange={handleJugadorElegido}>
-                        <option value="player">Seleccione un jugador</option>
-                        {options}
-                    </select>
-                </View>    
+
+                    <Select selectedValue={jugadorElegido} 
+                    minWidth="200" 
+                    accessibilityLabel="Jugadores" 
+                    placeholder="Seleccionar jugador" 
+                    mt={1} 
+                    onValueChange={(itemValue) => {console.log("itemValue: " + itemValue)}}>
+                                
+                    {jugadoresItems}
+                </Select>
+                </View>
+ 
             </Modal> 
+            </NativeBaseProvider>
         </View>
     )
 }
