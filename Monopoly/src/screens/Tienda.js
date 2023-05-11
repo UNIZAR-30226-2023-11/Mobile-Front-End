425efe8593e7eb92d7d8d4aa223ed235575a22b3
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2023-05-11 22:13:52 +0200

    Tienda implementada con bd

--- Monopoly/src/screens/Tienda.js
+++ Monopoly/src/screens/Tienda.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { View, StyleSheet , Text , Button, Pressable, Image, FlatList, TouchableOpacity, ScrollView} from'react-native';
+//import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import { View, StyleSheet , Text , Button, Pressable, Image, FlatList, TouchableOpacity, ScrollView, SafeAreaView} from'react-native';
 //import { unirPartida } from '../url/partida';
 import { SocketContext } from '../components/SocketContext';
 import { encode } from 'base-64';
@@ -81,39 +82,6 @@ const styles = StyleSheet.create({
       },
   })
 
-//se podria poner tambien una barra con nombre user y dinero €€€
-//falta añadir funcionalidad al boton
-const renderItem = ({item}) => {
-  const img = require('../../assets/bob.png')
-  return (
-    
-    <View style={styles.itemContainer}>
-      <Image style={styles.itemImage} source={img} />
-      <Text style={styles.itemText}>{item.nombre}</Text>
-      
-      {/*si aun no esta comprado*/}
-      {console.log("item comprado: " + item.comprado)}
-      {!item.comprado && (
-        <View>
-          <Text style={styles.itemPrecio}>€{item.precio}</Text>
-          <TouchableOpacity style={styles.itemButton}>
-            <Text style={styles.itemButtonText}>Comprar</Text>
-          </TouchableOpacity>
-        </View>  
-      )}
-      {/*si ya esta comprado*/}
-      {item.comprado && !item.usado && (
-          <TouchableOpacity style={styles.itemButton}>
-          <Text style={styles.itemButtonText}>Usar</Text>
-        </TouchableOpacity>
-      )}
-      {/*si ya esta comprado y ademas esta en uso*/}
-      {item.comprado && item.usado && (
-          <Text>Actual</Text>
-      )}
-    </View>
-); };
-
 const Header = ({ username, money }) => {
     return (
       <View style={styles.headerContainer}>
@@ -139,7 +107,6 @@ export default function TiendaScreen({ route, navigation }){
       console.log('Server acknowledged:', ack);
       if (ack.cod === 0) {
         fichas = ack.msg.map(item => ({
-          id: 0,
           nombre: item.nombre,
           precio: item.precio,
           usado: item.usado,
@@ -157,22 +124,74 @@ export default function TiendaScreen({ route, navigation }){
         alert("Se ha producido un error en el servidor. Salga del perfil y vuelva a entrar");
       }
     });
+
+
+    const renderItem = useCallback(({item}) => {
+      const img = require('../../assets/bob.png');
+    
+      return (
+        <View style={styles.itemContainer}>
+          <Image style={styles.itemImage} source={`data:image/jpg;base64,${item.imagen}`} />
+          <Text style={styles.itemText}>{item.nombre}</Text>
+    
+          {/*si aun no esta comprado*/}
+          {console.log("item comprado: " + item.comprado)}
+          {!item.comprado && (
+            <View>
+              <Text style={styles.itemPrecio}>€{item.precio}</Text>
+              <TouchableOpacity style={styles.itemButton} onPress={() => {
+                socket.emit('comprarTienda', {socketId: socket.id, producto: item.nombre}, (ack) => {
+                  console.log('Server acknowledged:', ack);
+                });
+              }}>
+                <Text style={styles.itemButtonText}>Comprar</Text>
+              </TouchableOpacity>
+            </View>  
+          )}
+    
+          {/*si ya esta comprado*/}
+          {item.comprado && !item.usado && item.nombre.startsWith("Avatar") && (
+            <TouchableOpacity style={styles.itemButton} onPress={() => {
+              socket.emit('updateImagenPerfil', {socketId: socket.id, imagen: item.imagen}, (ack) => {
+                console.log('Server acknowledged:', ack);
+              });
+            }}>
+              <Text style={styles.itemButtonText}>Usar</Text>
+            </TouchableOpacity>
+          )}
+    
+          {item.comprado && !item.usado && item.nombre.startsWith("Ficha") && (
+            <TouchableOpacity style={styles.itemButton} //onPress={
+              //socket.emit('updateImagen', {socketId: socket.id, producto: item.nombre}, (ack) => {
+                /*console.log('Server acknowledged:', ack);})} */>
+              <Text style={styles.itemButtonText}>Usar</Text>
+            </TouchableOpacity>
+          )}
+    
+          {/*si ya esta comprado y ademas esta en uso*/}
+          {item.comprado && item.usado && (
+            <Text>Actual</Text>
+          )}
+        </View>
+      );
+    }, [socket]);
+    
     
     
     return (
-        <ScrollView stickyHeaderIndices={[0]}>
-        <Header username={'@' + username} money={100 + 'M'} />
+      <SafeAreaView style={{flex: 1}}>
+        <Header username={'@' + username} money={40 + ' M'} />
             <View>
                 <View>
                     <Text style={styles.texto}>Fichas y avatares</Text>
                     <FlatList
                         data={fichas}
                         renderItem={renderItem}
-                        keyExtractor={(item) => item.id.toString()}
                         numColumns={3}
+                        socket={socket}
                     />
                     </View>
             </View>    
-        </ScrollView>        
+      </SafeAreaView>       
     );
 }
